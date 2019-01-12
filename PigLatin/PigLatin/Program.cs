a9837803e98aaf1b8bc7d043d91c60d8e27c5124
ginaisvor <ginaisvor@yahoo.com>
2019-01-12 20:11:57 +0200

    initial commit

---
+++ PigLatin/PigLatin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PigLatin
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string text = "Astazi, e - ziua. -/   mea., zi/ - // frumoasa .,ca - ,.mineeee!";
+            PigLatin test = new PigLatin(text);
+            Console.WriteLine(text);
+            test.Modify();
+
+            Console.WriteLine("Enter your text: ");
+            string text2 = Console.ReadLine();
+            PigLatin test2 = new PigLatin(text2);
+            test2.Modify();
+
+        }
+    }
+}
